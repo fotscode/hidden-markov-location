776f8bac6cfa131b6a67ce731350c4ead248aa6a
fots <facundotomatis@gmail.com>
2024-04-01 19:57:53 -0300

    reduced observations size

--- src/components/Observations.tsx
+++ src/components/Observations.tsx
@@ -4,9 +4,9 @@ type ObservationsProps = {
 export default function Observations({ observations }: ObservationsProps) {
   console.log(observations.length === 0)
   return (
-    <div className='flex flex-col justify-center mt-5 items-center'>
-      <p  hidden={observations.length === 0} className='text-4xl font-bold text-black'>Observations:</p>
-      <ul className='mt-2 w-full text-center rounded-lg bg-gray-800'>
+    <div className='flex flex-col justify-center my-5 items-center overflow-y-hidden h-2/6'>
+      <p  hidden={observations.length === 0} className='text-3xl font-bold'>Observations</p>
+      <ul className='mt-2 w-full text-center rounded-lg bg-gray-800 overflow-y-auto'>
         {observations.map((obs, i) => (
           <li key={i} className='text-lg p-2 border rounded-md'>
             {obs}
