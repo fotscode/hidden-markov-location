2cb018330a2be367d7a4fcee590c32006b7040fd
fots <facundotomatis@gmail.com>
2024-04-14 19:08:35 -0300

    modified buttons placements, sizing, colors and text

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -31,22 +31,11 @@ export default function Sidebar({
   }
   return (
     <div hidden={hidden}>
-      <section className='absolute inset-y-0 right-0 max-w-full flex m-0'>
+      <section className='fixed inset-y-0 right-0 max-w-full flex m-0'>
         <div className='w-screen max-w-md bg-gray-500 bg-opacity-50 backdrop-blur-sm shadow-xl'>
           <div className='h-full flex flex-col pt-4 px-4 justify-between'>
             <div className="max-h-full">
-              <div className='flex items-center justify-end'>
-                <Button
-                  isIconOnly
-                  variant='light'
-                  onClick={() => setHidden(true)}
-                >
-                  <span className='sr-only'>Close</span>
-                  <CloseIcon color={'#fff'} />
-                </Button>
-              </div>
-              <section className='flex flex-col justify-center items-center gap-1 mt-3'>
-                <div className='flex justify-between w-full gap-3'>
+              <div className='flex items-center justify-end gap-3'>
                   <Button
                     className='w-full'
                     color='secondary'
@@ -54,7 +43,7 @@ export default function Sidebar({
                       window.location.reload()
                     }}
                   >
-                    Reset
+                    Reiniciar
                   </Button>
                   <Button
                     className='w-full'
@@ -65,11 +54,23 @@ export default function Sidebar({
                   >
                     Reposicionar
                   </Button>
-                </div>
+                <Button
+                  isIconOnly
+                  variant='solid'
+                  color='secondary'
+                  onClick={() => setHidden(true)}
+                >
+                  <span className='sr-only'>Close</span>
+                  <CloseIcon color={'#fff'} />
+                </Button>
+              </div>
+              <section className='flex flex-col justify-center items-center gap-1 mt-3 mx-1'>
+                <div className='flex w-full gap-3 justify-center items-center'>
+
                 <div className="flex flex-col gap-6 w-full max-w-md">
                   <Slider
-                    key={"danger"}
-                    color={"danger"}
+                    key={"secondary"}
+                    color={"secondary"}
                     step={0.01}
                     maxValue={0.99}
                     minValue={0}
@@ -85,14 +86,16 @@ export default function Sidebar({
                   min='0'
                   max='99'
                   label='Error'
-                  value={error * 100}
+                  value={parseFloat(error * 100).toFixed(0)}
                   onChange={handleErrorChange}
+                  className='w-fit'
                   endContent={
                     <div className='pointer-events-none flex items-center'>
                       <span className='text-default-700 '>%</span>
                     </div>
                   }
                 />
+                </div>
               </section>
               <Observations observations={observations} />
             </div>
